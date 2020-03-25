6143915b34c688e22439a54ca2dfe8fb9367597a
antoni-wojcik <antekwojcik2@gmail.com>
2020-03-25 04:03:28 +0100

    updated main

--- main.cpp
+++ main.cpp
@@ -6,206 +6,220 @@
 //  Copyright © 2020 Antoni Wójcik. All rights reserved.
 //
 
+#define RETINA
+#define SCR_WIDTH 900
+#define SCR_HEIGHT 900
+
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <random>
-#include <cmath>
-#include <vector>
 
-#define __CL_ENABLE_EXCEPTIONS
-#define CL_HPP_TARGET_OPENCL_VERSION 120
-#define CL_HPP_MINIMUM_OPENCL_VERSION 120
-#include "cl2.hpp"
+// include OpenGL libraries
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
+#include "gtc/type_ptr.hpp"
 
-struct pos{
-    int x, y, z;
-    pos() {}
-    pos(int xx, int yy, int zz) {
-        x = xx;
-        y = yy;
-        z = zz;
-    }
-};
+#include "shader.h"
+#include "screen.h"
+#include "camera.h"
+#include "compute_kernel.h"
 
-std::string load_source(const char* compute_path) {
-    std::string compute_code;
-    std::ifstream compute_file;
-    compute_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    
-    try {
-        compute_file.open(compute_path);
-        std::ostringstream compute_stream;
-        compute_stream << compute_file.rdbuf();
-        compute_file.close();
-        compute_code = compute_stream.str();
-    } catch(std::ifstream::failure e) {
-        std::cerr << "ERROR: CANNOT READ COMPUTE CODE" << std::endl;
-        exit(-1); //stop executing the program with the error code -1;
-    }
-    return compute_code;
-}
+
+// function declarations
+void framebufferSizeCallback(GLFWwindow*, int, int);
+void processInput(GLFWwindow*);
+void mouseCallback(GLFWwindow*, double, double);
+void mouseButtonCallback(GLFWwindow*, int, int, int);
+void scrollCallback(GLFWwindow*, double, double);
+
+#ifdef __APPLE__
+void macWindowFix(GLFWwindow* window);
+
+bool mac_fixed = false;
+#endif
+
+#ifdef RETINA
+// dimensions of the viewport (they have to be multiplied by 2 at the retina displays)
+unsigned int scr_width = SCR_WIDTH*2;
+unsigned int scr_height = SCR_HEIGHT*2;
+#else
+unsigned int scr_width = SCR_WIDTH;
+unsigned int scr_height = SCR_HEIGHT;
+#endif
+
+float scr_ratio = (float)scr_width/(float)scr_height;
+
+// variables used in the main loop
+float delta_time = 0.0f;
+float last_frame_time = 0.0f;
+
+// variables used in callbacks
+bool mouse_first_check = true;
+bool mouse_hidden = true;
+float mouse_last_x = scr_width / 2.0f;
+float mouse_last_y = scr_width / 2.0f;
+
+// fps counter variables
+float fps_sum = 0.0f;
+const int fps_steps = 5;
+int fps_steps_counter = 0;
+
+//screenshot variable
+bool taking_screenshot = false;
+
+//camera
+Camera camera(60.0f, glm::vec3(0, 0, -2));
+
 
 int main(int argc, const char* argv[]) {
-    int size = 100;
-    int iterations = 5;
-    int nodes[5] = {5, 10, 20, 25, 50};
-    cl_float persistence[5] = {5.0f, 10.0f, 10.0f, 10.0f, 10.0f};
-    cl_float blending[5] = {1.0f, 0.3f, 0.1f, 0.05f, 0.04f};
+    // initialize GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
     
-    float* image_result = new float[size*size*size];
+    #ifdef RETINA
+    GLFWwindow* window = glfwCreateWindow(scr_width/2, scr_height/2, "Clouds", NULL, NULL);
+    #else
+    GLFWwindow* window = glfwCreateWindow(scr_width, scr_height, "Clouds", NULL, NULL);
+    #endif
+    if(window == NULL) {
+        std::cout << "ERROR: OpenGL: Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
     
-    try {
-        cl::Device device;
-        std::vector<cl::Platform> platforms;
-        std::vector<cl::Device> devices;
-        cl::Platform::get(&platforms);
-        if(platforms.size() == 0) {
-            std::cerr << "ERROR: OPENCL: NO PLATFORMS FOUND" << std::endl;;
-        }
-        
-        platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
-        if(devices.size() == 0) {
-            std::cerr << "ERROR: OPENCL: NO DEVICES FOUND" << std::endl;
-        }
-        device = devices[1]; //choose the graphics card
-        
-        std::cout << "SUCCESS: OPENCL: USING A DEVICE: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
-        
-        cl::Context context({device});
-        cl::Program::Sources sources;
-        std::string kernel_code = load_source("src/compute_gpu/generate_3d_cloud.ocl");
-        sources.push_back({kernel_code.c_str(), kernel_code.length()});
-        
-        cl::Program computing_program(context, sources);
-        if (computing_program.build({device}) != CL_SUCCESS)
-        {
-            std::cout << "ERROR: OPENCL: CANNOT BUILD PROGRAM " << computing_program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << std::endl;
-            return -1;
-        }
-        
-        std::random_device dev;
-        std::mt19937 rng(dev()); //random number generator
-        
-        cl::ImageFormat image_format(CL_R, CL_FLOAT);
-        cl::Image3D image(context, CL_MEM_READ_WRITE, image_format, size, size, size); //CL_MEM_READ_WRITE
-        
-        cl::Buffer persistence_buff;
-        cl::Buffer blending_buff;
-        
-        cl::ImageFormat image_in_format(CL_RGBA, CL_UNSIGNED_INT32);
-        cl::Image3D vertices_image;
-        
-        cl::Kernel generate(computing_program, "generate");
-        
-        
-        for(int k = 0; k < iterations; k++) {
-            std::uniform_int_distribution<std::mt19937::result_type> distr(0,size/nodes[k]-1);
-            
-            int nodes_rep = nodes[k] + 2;
-            
-            pos*** v = new pos**[nodes_rep];
-            for(int i = 0; i < nodes_rep; i++) {
-                v[i] = new pos*[nodes_rep];
-                for(int j = 0; j < nodes_rep; j++) {
-                    v[i][j] = new pos[nodes_rep];
-                }
-            }
-            
-            for(int n = 1; n <= nodes[k]; n++) for(int j = 1; j <= nodes[k]; j++) for(int i = 1; i <= nodes[k]; i++) {
-                v[i][j][n].x = distr(rng);
-                v[i][j][n].y = distr(rng);
-                v[i][j][n].z = distr(rng);
-            }
-            
-            for(int n = 0; n < nodes_rep; n++) {
-                for(int i = 0; i < nodes_rep; i++) {
-                    int j = 1+(i-1+nodes[k])%nodes[k];
-                    int u = 1+(n-1+nodes[k])%nodes[k];
-                    v[i][0][n] =          v[j][nodes[k]][u];
-                    v[i][nodes[k]+1][n] = v[j][1][u];
-                    v[0][i][n] =          v[nodes[k]][j][u];
-                    v[nodes[k]+1][i][n] = v[1][j][u];
-                    v[i][n][0] =          v[j][u][nodes[k]];
-                    v[i][n][nodes[k]+1] = v[j][u][1];
-                }
-            }
-            
-            
-            cl_uint* vertices = new cl_uint[nodes_rep*nodes_rep*nodes_rep*4];
-            for(int n = 0; n < nodes_rep; n++) for(int j = 0; j < nodes_rep; j++) for(int i = 0; i < nodes_rep; i++) {
-                vertices[n*nodes_rep*nodes_rep*4 + j*nodes_rep*4 + i*4]     = v[i][j][n].x;
-                vertices[n*nodes_rep*nodes_rep*4 + j*nodes_rep*4 + i*4 + 1] = v[i][j][n].y;
-                vertices[n*nodes_rep*nodes_rep*4 + j*nodes_rep*4 + i*4 + 2] = v[i][j][n].y;
-            }
-            
-            for(int i = 0; i < nodes_rep; i++) {
-                for(int j = 0; j < nodes_rep; j++) {
-                    delete [] v[i][j];
-                }
-                delete [] v[i];
-            }
-            delete [] v;
-            
-            
-            vertices_image = cl::Image3D(context, CL_MEM_READ_ONLY, image_in_format, nodes_rep, nodes_rep, nodes_rep);
-            persistence_buff = cl::Buffer(context, CL_MEM_READ_ONLY, sizeof(cl_float));
-            blending_buff = cl::Buffer(context, CL_MEM_READ_ONLY, sizeof(cl_float));
-            
-            generate.setArg(0, vertices_image);
-            generate.setArg(1, image);
-            generate.setArg(2, image);
-            generate.setArg(3, blending[k]);
-            generate.setArg(4, persistence[k]);
-            
-            cl::CommandQueue queue(context, device);
-            
-            queue.enqueueWriteImage(vertices_image, CL_TRUE, {0, 0, 0}, {size_t(nodes_rep), size_t(nodes_rep), size_t(nodes_rep)}, 0, 0, vertices);
-            queue.enqueueWriteBuffer(blending_buff, CL_TRUE, 0, sizeof(cl_float), &blending[k]);
-            queue.enqueueWriteBuffer(persistence_buff, CL_TRUE, 0, sizeof(cl_float), &persistence[k]);
-            
-            queue.enqueueNDRangeKernel(generate, cl::NullRange, cl::NDRange(size_t(size), size_t(size), size_t(size)), cl::NullRange);
-            
-            if(k == iterations-1) queue.enqueueReadImage(image, CL_TRUE, {0, 0, 0}, {size_t(size), size_t(size), size_t(size)}, 0, 0, image_result);
-
-            queue.finish();
-            
-            delete [] vertices;
-        }
-        
-    } catch(cl::Error e) {
-        std::cerr << "ERROR: OPENCL: OTHER: " << e.what() << ": " << e.err() << std::endl;
-        if(e.err() == -11) {
-            std::cerr << "COMPILATION ERROR, CHECK THE SYNTAX\nUSE:\nhttps://streamhpc.com/blog/2013-05-13/verify-your-opencl-kernel-online/\nTO FIND ERRORS" << std::endl;
-        } else std::cerr << "USE:\nhttps://www.khronos.org/registry/OpenCL/sdk/1.2/docs/man/xhtml/scalarDataTypes.html\nTO VERIFY ERROR TYPE" << std::endl;
+    // set the viewport size and apply changes every time it is changed by a user
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    glfwSetCursorPosCallback(window, mouseCallback);
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    glfwSetScrollCallback(window, scrollCallback);
+    
+    // tell GLFW to capture the mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+    // initalize GLAD
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "ERROR: OpenGL: Failed to initialize GLAD" << std::endl;
         return -1;
     }
     
-    for(int k = 0; k < size; k++) {
-        std::string name("output/sim");
-        if(k < 10) name += "00";
-        else if(k < 100) name += "0";
-        name += std::to_string(k) + ".ppm";
-        std::ofstream img(name.c_str());
-        img << "P3\n" << size << " " << size << "\n255\n";
+    Shader shader("src/shaders/screen.vs", "src/shaders/clouds.fs");
+    
+    Screen screen;
+    
+    Clouds clouds(shader);
+    
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    while(!glfwWindowShouldClose(window)) {
+        #ifdef __APPLE__
+        macWindowFix(window);
+        #endif
         
-        for(int j = 0; j < size; j++) for(int i = 0; i < size; i++) {
-            int ir = int(255.99f*image_result[k*size*size + j*size + i]);
-            int ig = int(255.99f*image_result[k*size*size + j*size + i]);
-            int ib = int(255.99f*image_result[k*size*size + j*size + i]);
-            
-            img << ir << " " << ig << " " << ib << "\n";
+        float currentFrameTime = glfwGetTime();
+        delta_time = currentFrameTime - last_frame_time;
+        last_frame_time = currentFrameTime;
+        if(fps_steps_counter == fps_steps) {
+            fps_steps_counter = 0;
+            fps_sum = 0;
         }
+        fps_sum += delta_time;
+        fps_steps_counter++;
+        
+        processInput(window);
+        
+        camera.transferData(shader);
 
-        img.close();
+        clouds.transferData();
+        
+        shader.setFloat("time", currentFrameTime);
+        
+        screen.draw(shader);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
     
-    delete [] image_result;
-    
-    system("open output/sim000.ppm");
-    
-    //use "convert -delay 5 -loop 0 *.ppm  gif.gif" (ImageMagick) in terminal to generate gif from the files
-    
+    glfwTerminate();
     return 0;
 }
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+    scr_width = width;
+    scr_height = height;
+    scr_ratio = (float)width/(float)height;
+    
+    camera.setSize(scr_ratio);
+}
+
+void processInput(GLFWwindow* window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) camera.move(FORWARD, delta_time);
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) camera.move(BACK, delta_time);
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) camera.move(LEFT, delta_time);
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) camera.move(RIGHT, delta_time);
+    
+    if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) camera.setFasterSpeed(true);
+    else if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE) camera.setFasterSpeed(false);
+    if(glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) camera.setSlowerSpeed(true);
+    else if(glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_RELEASE && glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE) camera.setSlowerSpeed(false);
+    
+    if(glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS) {
+        if(!taking_screenshot) camera.takeScreenshot(scr_width, scr_height);
+        taking_screenshot = true;
+    } else if(glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_RELEASE) {
+        taking_screenshot = false;
+    }
+}
+
+void mouseCallback(GLFWwindow* window, double x_pos, double y_pos) {
+    if(mouse_first_check) {
+        mouse_last_x = x_pos;
+        mouse_last_y = y_pos;
+        mouse_first_check = false;
+    }
+    
+    float offset_x = x_pos - mouse_last_x;
+    float offset_y = mouse_last_y - y_pos;
+    
+    mouse_last_x = x_pos;
+    mouse_last_y = y_pos;
+    
+    if(mouse_hidden) camera.rotate(offset_x, offset_y);
+}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        if(mouse_hidden) {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        }
+        else glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        
+        mouse_hidden = !mouse_hidden;
+    }
+}
+
+void scrollCallback(GLFWwindow* window, double offset_x, double offset_y) {
+    camera.zoom(offset_y);
+}
+
+
+//for some reason on Mac OS 10.14+ OpenGL window will only display black color until it is resized for the first time. This function does it automatically
+#ifdef __APPLE__
+void macWindowFix(GLFWwindow* window) {
+    if(!mac_fixed) {
+        int x, y;
+        glfwGetWindowPos(window, &x, &y);
+        glfwSetWindowPos(window, ++x, y);
+        
+        glViewport(0, 0, scr_width, scr_height);
+        mac_fixed = true;
+    }
+}
+#endif
